e24d076b3cd12efe9bbd7c748a85a31b22d4b2c2
Zhijia <zhijia.hu@ubisoft.com>
2017-12-07 09:45:46 +0800

    tidy up the output in console

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scope
 A simple monitoring tool
+
+### Running the examples
+    $ python main.py -s ../examples
--- scope/main.py
+++ scope/main.py
@@ -18,13 +18,12 @@ args = vars(ap.parse_args())
 
 
 async def request(session, url):
-    print('[INFO] Checking {}'.format(url))
+    print('[INFO] [{}] IN PROGRESS...'.format(url))
     with async_timeout.timeout(10):
         async with session.get(url) as response:
             return await response.text()
 
 async def main():
-
     scenario_files = glob.glob('{0}/*.yml'.format(args['scenarios_dir']))
 
     for path in scenario_files:
@@ -34,8 +33,9 @@ async def main():
         for scenario in config['scenarios']:
             async with aiohttp.ClientSession() as session:
                 base_url = config['backends'][scenario['backend']]
-                result = await request(session, urljoin(base_url, scenario['url']))
-                print(result)
+                url = urljoin(base_url, scenario['url'])
+                result = await request(session, url)
+                print('[INFO] [{}] COMPLETED: {}'.format(url, result))
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
